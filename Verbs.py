926368557cc126e2ca5c78c1da84d1208135be76
Worf <31394095+rednepdev@users.noreply.github.com>
2018-02-13 12:37:40 +0000

    Update Verbs.py

--- Verbs.py
+++ Verbs.py
@@ -1,3 +1,4 @@
+import time
 def make_3gs_form(verb):
     verblength = int(len(verb) - 1) ##finds length of inputed word, min 1 because indexes
     if verb[verblength] == "y": ##if last leter is y
@@ -8,6 +9,16 @@ def make_3gs_form(verb):
     else:
         newverb = verb + "s"
     return(newverb) ##output
+def make_ing_form(verb):
+    verblength = int(len(verb) - 1) ##finds length of inputed word, min 1 because indexes
+    if verb[verblength] == "e":
+        newverb = verb[:verblength]
+        newverb = newverb + "ing"
+    elif verb[verblength] == "ie":
+        newverb = verb[:(verblength - 1)]
+        newverb = verb + "ying"
+    return(newverb)
 verb = input("Enter verb to make plural ")
 verb = make_3gs_form(verb) ##sets verb equal to function
 print(verb) ##prints output
+time.sleep(4)
